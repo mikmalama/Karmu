d280998e544743c9cbb3b5167dabee74017a8404
mikmalama <122720780+mikmalama@users.noreply.github.com>
2023-01-15 14:40:49 +0545

    Add files via upload

---
+++ javascript/one.js
@@ -0,0 +1,50 @@
+var name='Mikma'
+console.log(name)
+console.log(name)
+console.log(name)
+var name='Lama'
+console.log(name)
+let age=21
+console.log(age)
+ age=21
+console.log(age)
+const abc=66
+console.log(abc)
+//data taaypen string number boolean, undefined,object, null, array, object
+var name='MIkma'
+console.log(typeof name);
+
+var ww=33.33
+console.log(typeof age)
+
+var work=true
+console.log(typeof work)
+
+var addtes1s=null
+console.log(typeof address)
+
+var friends=['Asmits','Alisha','Khushi']
+console.log(typeof friends)
+
+var obj={
+    id:1, name: "Mikma", address: "Gokarneshwor"
+
+}
+console.log(typeof obj)
+//es6 bigInt, Symbol
+var bca=123443563547645
+console.log(BigInt(bca))
+//symbol
+var q=Symbol('Mikma');
+var w=Symbol('Mikma');
+console.log(q==w)
+
+
+
+
+
+
+
+
+
+
